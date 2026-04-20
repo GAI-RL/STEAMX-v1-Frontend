24391c90169aac9bdd854d454fa373f3396c4c19
AIRL <airl@kics.edu.pk>
2026-04-20 12:35:14 +0500

    resolve session routing

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -2,10 +2,12 @@ import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import { catchError, EMPTY, throwError } from 'rxjs';
+import { SessionService } from '../services/session-service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+    const sessionService = inject(SessionService);
   const router = inject(Router);
   const token = authService.getToken();
 
@@ -23,8 +25,10 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     catchError((error: HttpErrorResponse) => {
       // If we get 401 (Unauthorized), token expired or invalid
       if (error.status === 401) {
-        authService.logout();  // Log user out
-        router.navigate(['/login']);  // Send to login page
+        // authService.logout();  // Log user out
+        // router.navigate(['/login']);  // Send to login page
+         sessionService.triggerSessionExpired()      // ← no inline banner
+        return EMPTY;   
       }
       return throwError(() => error);
     })

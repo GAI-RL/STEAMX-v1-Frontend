26a98e95197385dd17d646b4a31b948f75503de0
Aiemen Altaf <airl@kics.edu.pk>
2026-06-18 15:47:25 +0500

    Fix Angular chat upload and message output issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:9000/api',
+  //apiUrl: 'http://127.0.0.1:9000/api',
   //apiUrl: 'http://127.0.0.1:8000/api',  // Your local FastAPI backend
-  //apiUrl: 'https://steamx-v1-backend.onrender.com/api',//
+  apiUrl: 'https://steamx-v1-backend.onrender.com/api',
   ocrApiUrl: 'https://ocr-9e8w.onrender.com',
   
   googleClientId: '1029552044709-ddm5ejh52929u89bbuj9e7m19q74d8ao.apps.googleusercontent.com'

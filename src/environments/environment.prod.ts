0f4ffa893b6bd3978bed56cc22adf2f758edc8bd
Khair ul Wara <mynameiskhairulwara@gmail.com>
2026-06-23 13:10:27 +0500

    trancribe api url issue reolved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://steamx-v1-backend.onrender.com/api',
   ocrApiUrl: 'https://ocr-9e8w.onrender.com',
+  transcribeApiUrl: 'https://speech-to-text-service-36jb.onrender.com/api/transcribe',
   googleClientId: '1029552044709-ddm5ejh52929u89bbuj9e7m19q74d8ao.apps.googleusercontent.com'
 };

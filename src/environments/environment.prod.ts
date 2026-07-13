b5b2317da526e9c0c3145f41a0ca5f0f239872a6
Khair ul Wara <mynameiskhairulwara@gmail.com>
2026-07-13 15:21:36 +0500

    OCR updated + response issue fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://steamx-v1-backend.onrender.com/api',
-  ocrApiUrl: 'https://ocr-9e8w.onrender.com',
+  ocrApiUrl: 'https://universal-text-extractor.onrender.com',
   transcribeApiUrl: 'https://speech-to-text-service-36jb.onrender.com/api/transcribe',
   googleClientId: '1029552044709-ddm5ejh52929u89bbuj9e7m19q74d8ao.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/api',
-  ocrApiUrl: 'https://ocr-9e8w.onrender.com',
+  ocrApiUrl: 'https://universal-text-extractor.onrender.com',
   transcribeApiUrl: 'https://speech-to-text-service-36jb.onrender.com/api/transcribe',
   googleClientId: '1029552044709-ddm5ejh52929u89bbuj9e7m19q74d8ao.apps.googleusercontent.com'
 };
